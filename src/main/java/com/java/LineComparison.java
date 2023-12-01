aae216a069d497fe318ef62a44a7cb8cd3218f65
Asish <asishkumar717@gmail.com>
2023-12-01 10:38:39 +0530

    Added line comparsion by greater,less and equal

--- src/main/java/com/java/LineComparison.java
+++ src/main/java/com/java/LineComparison.java
@@ -15,14 +15,17 @@ public class LineComparison {
         int y2=sc.nextInt();
         int x3=2,x4=3,y3=2,y4=3;
         float line1=(float)Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-        System.out.println("Length of the line = "+line1);
+        System.out.println("Length of the line1 = "+line1);
         float line2=(float)Math.sqrt(Math.pow(x4-x3,2)+Math.pow(y4-y3,2));
-        System.out.println("Length of the line = "+line2);
+        System.out.println("Length of the line2 = "+line2);
         if (line1==line2){
             System.out.println("Lines are equal");
         }
-        else {
-            System.out.println("Lines are not equal");
+        else if (line1>line2){
+            System.out.println("Line1 is greater then Line2");
+        }
+        else if (line1<line2){
+            System.out.println("Line1 is less then Line2");
         }
     }
 }
